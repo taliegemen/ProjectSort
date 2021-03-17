ef2b973e7cf5feba4aeaa4b641d6bc3f28dd4f5a
Ali Egemen Taşören <taliegemen@gmail.com>
2021-03-17 11:39:40 +0300

    Update ProjectPySort.py

--- Code/Python/ProjectPySort.py
+++ Code/Python/ProjectPySort.py
@@ -201,9 +201,9 @@ while True:
         graph.Erase()
         for k in enumerate(Array):
             graph_value = Array[k]
-            graph.DrawRectangle(top_left=(k * BAR_SPACING + EDGE_OFFSET, graph_value),
-            bottom_right=(k * BAR_SPACING + EDGE_OFFSET 
-            + BAR_WIDTH, 0), fill_color='light blue')
+            graph.DrawRectangle(top_left=(k * BAR_SPACING + EDGE_OFFSET, graph_value), 
+                bottom_right=(k * BAR_SPACING + EDGE_OFFSET 
+                + BAR_WIDTH, 0), fill_color='light blue')
             graph.DrawText(text=graph_value, location=(k * BAR_SPACING 
                 + EDGE_OFFSET + 25, graph_value + 10))
     if event == 'Bubble':
